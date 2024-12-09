21009da43166ac640c3d25596d5e3d62ce272d7c
Mohammadosama55 <152876079+Mohammadosama55@users.noreply.github.com>
2024-12-09 17:47:33 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,13 @@
+function Greet() {
+    alert("hello, world");
+}
+
+// for each square
+function Square(color, id, piece) {
+    return { color, id, piece};
+}
+
+// when game is start
+function initGame() {}
+
+export { Square };
